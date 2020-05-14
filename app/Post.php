4b118c20ede519a43fd522ae2c1338f96a88ef17
suryamunirathinam <suryamunirathinam@gmail.com>
2020-05-14 17:00:50 +0530

    polymorphic relationships

---
+++ app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $guarded =[];
+    public function image()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
+
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::Class,'commentable');
+    }
+
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class,'taggable');
+    }
+}
